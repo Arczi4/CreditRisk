085f1264bb9ecfd5b3bda8cea7c42a2165654b72
Arczi4 <urzedowski.artur@gmail.com>
2026-02-24 18:20:29 +0100

    added basic app settings, added run.py file

---
+++ apps/backend/app/core/settings.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api"
+
+    # Server Config
+    HOST: str = "0.0.0.0"
+    PORT: int = 8000
+    DEBUG: bool = True
+
+    # Cors config
+    CORS_ORIGIN: str = "http://localhost:4200"  # Default angular
+
+    # Logging config
+    LOG_DIR: str = "logs"
+
+    @property
+    def LOG_LEVEL(self) -> str:
+        return "debug" if self.DEBUG else "info"
+
+
+settings = Settings()
