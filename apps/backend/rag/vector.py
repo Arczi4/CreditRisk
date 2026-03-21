da79fe8fb4761d3df82ce9113f52cb0040b00702
Arczi4 <urzedowski.artur@gmail.com>
2026-03-21 21:13:33 +0100

    Init Langchain with chromadb

---
+++ apps/backend/rag/vector.py
@@ -0,0 +1,53 @@
+import os
+import glob
+from pathlib import Path
+from dotenv import load_dotenv
+
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import (
+    UnstructuredMarkdownLoader,
+    DirectoryLoader,
+    TextLoader,
+)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+current_dir = Path(__file__).parent
+docs_path = current_dir / "docs"
+db_path = current_dir / "chrome_db"
+
+print(f"Docs path: {docs_path}, ChromaDB path: {db_path}")
+
+add_documents = False if os.path.exists(db_path) else True
+
+if add_documents:
+    documents = []
+    ids = []
+
+    loader = DirectoryLoader(
+        path=docs_path,
+        glob="**/*.md",
+        loader_cls=TextLoader,
+        loader_kwargs={"encoding": "utf-8"},
+        show_progress=True,
+    )
+
+    docs = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = splitter.split_documents(docs)
+
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        collection_name="knowledge-base",
+        persist_directory=db_path,
+    )
+
+    print(f"Loaded {len(chunks)} chunks to Chroma")
+else:
+    print("Did not load any document to Chroma. Chroma already created")
