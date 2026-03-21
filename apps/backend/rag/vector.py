321b7131de2db1ce3b8673b6fb50901aae10f4b3
Arczi4 <urzedowski.artur@gmail.com>
2026-03-21 21:42:19 +0100

    vector code improvement, added retriever

--- apps/backend/rag/vector.py
+++ apps/backend/rag/vector.py
@@ -1,11 +1,9 @@
 import os
-import glob
 from pathlib import Path
 from dotenv import load_dotenv
 
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import (
-    UnstructuredMarkdownLoader,
     DirectoryLoader,
     TextLoader,
 )
@@ -17,15 +15,13 @@ load_dotenv()
 current_dir = Path(__file__).parent
 docs_path = current_dir / "docs"
 db_path = current_dir / "chrome_db"
-
 print(f"Docs path: {docs_path}, ChromaDB path: {db_path}")
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
 add_documents = False if os.path.exists(db_path) else True
 
 if add_documents:
-    documents = []
-    ids = []
-
     loader = DirectoryLoader(
         path=docs_path,
         glob="**/*.md",
@@ -36,12 +32,12 @@ if add_documents:
 
     docs = loader.load()
 
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200, add_start_index=True
+    )
     chunks = splitter.split_documents(docs)
 
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-
-    vectorstore = Chroma.from_documents(
+    vector_store = Chroma.from_documents(
         documents=chunks,
         embedding=embeddings,
         collection_name="knowledge-base",
@@ -51,3 +47,10 @@ if add_documents:
     print(f"Loaded {len(chunks)} chunks to Chroma")
 else:
     print("Did not load any document to Chroma. Chroma already created")
+    vector_store = Chroma(
+        collection_name="restaurant_reviews",
+        persist_directory=db_path,
+        embedding_function=embeddings,
+    )
+
+retriever = vector_store.as_retriever(search_kwargs={"k": 5})
